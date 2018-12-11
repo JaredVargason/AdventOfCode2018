8923112e7f6f1eeeac445febae43bcab845078c2
Jared Vargason <jaredvargason@gmail.com>
2018-12-10 19:07:43 -0500

    finished day10

--- day10/message.py
+++ day10/message.py
@@ -2,6 +2,12 @@ import time
 import re
 import operator
 
+currentX = 0
+currentY = 0
+last = '' 
+step = 20
+grid_height = 30
+grid_width = 30
 
 seconds = 0
 coords = []
@@ -13,7 +19,7 @@ with open('input.txt', 'r') as f:
         coords.append((int(matches.group(1)), int(matches.group(2))))
         velocities.append((int(matches.group(3)), int(matches.group(4))))
 
-def step(n):
+def step_forward(n):
     global coords, velocities
     if n == 1:
         coords = [(coords[i][0] + velocities[i][0],coords[i][1] + velocities[i][1]) for i in range(len(coords))]
@@ -37,28 +43,22 @@ def print_grid(coord_x, coord_y, grid_width, grid_height):
     global seconds 
     grid = [['X' for x in range(grid_width)] for y in range(grid_height)]
 
-    strings = []    
-    for y in range(len(grid)):
-        string = ' '.join(grid[y]) 
-        strings.append(string)
-
-    print(seconds)
-
-    minX = min(coords, key= lambda t: t[0])[0]
-    maxX = max(coords, key= lambda t: t[0])[0]
-    minY = min(coords, key= lambda t: t[1])[1]
-    maxY = max(coords, key= lambda t: t[1])[1]
     for coord in coords:
-            if coord[0] in range(coord_x, coord_x + grid_width) and coord[1] in range(coord_y, coord_y + grid_height):
-                grid[coord[1] - minY][coord[0] - minX] = ' '
+        if coord[0] in range(coord_x, coord_x + grid_width) and coord[1] in range(coord_y, coord_y + grid_height):
+            try:
+                grid[coord[1] - coord_y][coord[0] - coord_x] = ' '
+            
+            except:
+                pass
 
     strings = []    
     for y in range(len(grid)):
         string = ''.join(grid[y]) 
         strings.append(string)
 
-    print('\n'.join(strings)) 
 
+
+    print('\n'.join(strings)) 
 def measure_closeness():
     total_dist = 0 
     global coords
@@ -70,41 +70,40 @@ def measure_closeness():
                 total_dist += abs(coord1[0] - coord2[0]) + abs(coord1[1] - coord2[1])
 
     return total_dist
-            
-currentX = 0
-currentY = 0
+
 while True:
-    string = input('Enter number of steps, 1 to n. "x" to leave\n')
-    if string == 'x':
+    command = input('Enter (s)teps, (c)oordinates, (p)rint current grid, (wsad) move the map, (r)edo last command, 1 to n. "x" to leave\n')
+    if command == 'x':
         break
-    elif string == 'r':
-        step(last)
-    elif string == 'c':
+    elif command == 'r':
+        step_forward(last)
+    elif command == 'c':
         print(coords)
-    elif string == 'p':
+    elif command == 'p':
         print_grid(currentX, currentY, 30, 30)
-    elif string == 'w':
-        currentY += 1
+    elif command == 'w':
+        currentY += step
         print_grid(currentX, currentY, 30, 30)
-    elif string == 's':
-        currentY -= 1
+    elif command == 's':
+        currentY -= step 
         print_grid(currentX, currentY, 30, 30)
-    elif string == 'a':
-        currentX += 1
+    elif command == 'a':
+        currentX += step 
         print_grid(currentX, currentY, 30, 30)
-    elif string == 'd':
-        currentX -= 1
+    elif command == 'd':
+        currentX -= step 
         print_grid(currentX, currentY, 30, 30)
     
     else:
         try:
-            steps = int(string)
-        except:
+            steps = int(command)
+        except ValueError:
             steps = 0 
-        step(steps)
+        step_forward(steps)
         last = steps
         
     dist = measure_closeness()
+    print('x', currentX, currentY, )
     print('seconds:', seconds, 'dist:', dist)
 
     
